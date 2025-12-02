afd0d3319ef1c3aaacfaa335e17f25197a8c9275
Rook <rook@trashbird.gay>
2025-12-01 18:47:46 -0600

    change remark42 site id

--- home/gloomlets/remark/remark.js
+++ home/gloomlets/remark/remark.js
@@ -2,7 +2,7 @@ class Remark {
 	constructor() {
 		window.remark_config = {
 			host: "https://remark.trashbird.gay",
-			site_id: "Domak",
+			site_id: "Gloomjam",
 			name: "comments",
 			components: ["embed", "last-comments"],
 			max_shown_comments: 20,
