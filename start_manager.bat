f602dbfa37d9bb2553208f118e9fe2666c1e55dc
Rook <rook@trashbird.gay>
2026-05-30 18:00:19 -0500

    Update arrow.clip, button2.clip, chapter title format.clip, and 12 more files

---
+++ start_manager.bat
@@ -0,0 +1,32 @@
+@echo off
+IF EXIST "busybox.exe" (
+	ECHO Busybox already downloaded
+	GOTO runbusybox
+) ELSE (
+	ECHO Downloading Busybox
+	curl --output busybox.exe --url https://frippery.org/files/busybox/busybox.exe
+	GOTO runbusybox
+)
+
+:runbusybox
+ECHO Running busybox
+START ./busybox.exe httpd -fvv -h home
+timeout 2
+ECHO Opening index page
+START "" "http://localhost/"
+
+
+IF EXIST "Gloomjam Manager.exe" (
+	ECHO Gloomjam Manager already downloaded
+	GOTO rungm
+) ELSE (
+	ECHO Downloading Gloomjam Manager
+	curl --output "gm_win.zip" --url https://files.trashbird.gay/seafhttp/f/27bff616919c4ef6b529/?op=view
+	ECHO Extracting Gloomjam Manager
+	tar -xf "gm_win.zip"
+	GOTO rungm
+)
+
+:rungm
+ECHO running Gloomjam Manager
+START ./"Gloomjam Manager.exe"
